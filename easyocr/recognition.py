335ec3d6127da8fda65abdea64920409aee66643
Piyawat Anugoolsawat <piyawat.anugool@gmail.com>
2022-06-09 16:53:36 +0700

    update config testing

--- easyocr/recognition.py
+++ easyocr/recognition.py
@@ -163,6 +163,7 @@ def get_recognizer(recog_network, network_params, character,\
         model_pkg = importlib.import_module("easyocr.model.vgg_model")
     else:
         model_pkg = importlib.import_module(recog_network)
+        print(recog_network)
     model = model_pkg.Model(num_class=num_class, **network_params)
 
     if device == 'cpu':
--- my_model/user_network/rosetta.py
+++ my_model/user_network/rosetta.py
@@ -1,36 +1,105 @@
 import torch.nn as nn
+from modules.transformation import TPS_SpatialTransformerNetwork
+from modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from modules.sequence_modeling import BidirectionalLSTM
+from modules.prediction import Attention
+
+import yaml
+import os
 # import importlib.util
 # import sys
 # spec = importlib.util.spec_from_file_location("feature_extraction", "trainer\modules\\feature_extraction.py")
 # feature_ex = importlib.util.module_from_spec(spec)
 # spec.loader.exec_module(feature_ex)
-from feature_extraction import ResNet_FeatureExtractor
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def get_config(file_path):
+    with open(file_path, 'r', encoding="utf8") as stream:
+        opt = yaml.safe_load(stream)
+    opt = AttrDict(opt)
+
+    return opt
+#     { 'Transformer': opt.Transformation,
+#             'FeatureExtraction': opt.FeatureExtraction,
+#             'SequenceModeling': opt.SequenceModeling,
+#             'Prediction': opt.Prediction,
+# }
+
 
 class Model(nn.Module):
+    
     def __init__(self, input_channel, output_channel, hidden_size, num_class):
         super(Model, self).__init__()
+        path = os.path.join('my_model', 'user_network', 'rosetta.yaml')
+        opt = get_config(path)
+        self.opt = opt
+        self.stages = {'Trans': opt.Transformation, 'Feat': opt.FeatureExtraction,
+                    'Seq': opt.SequenceModeling, 'Pred': opt.Prediction}
+
+        """ Transformation """
+        print(f'Transformation: {opt.Transformation}')
+        if opt.Transformation == 'TPS':
+            self.Transformation = TPS_SpatialTransformerNetwork(
+                F=opt.num_fiducial, I_size=(opt.imgH, opt.imgW), I_r_size=(opt.imgH, opt.imgW), I_channel_num=input_channel)
+
         """ FeatureExtraction """
-        self.FeatureExtraction = ResNet_FeatureExtractor(input_channel, output_channel)
-        self.FeatureExtraction_output = output_channel
-        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))
+        print(f'FeatureExtraction: {opt.FeatureExtraction}')
+        if opt.FeatureExtraction == 'VGG':
+            self.FeatureExtraction = VGG_FeatureExtractor(input_channel, output_channel)
+        elif opt.FeatureExtraction == 'RCNN':
+            self.FeatureExtraction = RCNN_FeatureExtractor(input_channel, output_channel)
+        elif opt.FeatureExtraction == 'ResNet':
+            self.FeatureExtraction = ResNet_FeatureExtractor(input_channel, output_channel)
+        # else:
+        #     raise Exception('No FeatureExtraction module specified')
+        self.FeatureExtraction_output = output_channel  # int(imgH/16-1) * 512
+        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
 
         """ Sequence modeling"""
-        self.SequenceModeling_output = self.FeatureExtraction_output
+        print(f'SequenceModeling: {opt.SequenceModeling}')
+        if opt.SequenceModeling == 'BiLSTM':
+            self.SequenceModeling = nn.Sequential(
+                BidirectionalLSTM(self.FeatureExtraction_output, hidden_size, hidden_size),
+                BidirectionalLSTM(hidden_size, hidden_size, hidden_size))
+            self.SequenceModeling_output = hidden_size
+        else:
+            # print('No SequenceModeling module specified')
+            self.SequenceModeling_output = self.FeatureExtraction_output
 
         """ Prediction """
-        self.Prediction = nn.Linear(self.SequenceModeling_output, num_class)
+        print(f'Prediction: {opt.Prediction}')
+        if opt.Prediction == 'CTC':
+            self.Prediction = nn.Linear(self.SequenceModeling_output, num_class)
+        elif opt.Prediction == 'Attn':
+            self.Prediction = Attention(self.SequenceModeling_output, hidden_size, num_class)
+        else:
+            raise Exception('Prediction is neither CTC or Attn')
 
+    def forward(self, input, text, is_train=True):
+        """ Transformation stage """
+        if not self.stages['Trans'] == "None":
+            input = self.Transformation(input)
 
-    def forward(self, input, text):
         """ Feature extraction stage """
         visual_feature = self.FeatureExtraction(input)
-        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))
+        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
         visual_feature = visual_feature.squeeze(3)
 
         """ Sequence modeling stage """
-        contextual_feature = visual_feature
+        if self.stages['Seq'] == 'BiLSTM':
+            contextual_feature = self.SequenceModeling(visual_feature)
+        else:
+            contextual_feature = visual_feature  # for convenience. this is NOT contextually modeled by BiLSTM
 
         """ Prediction stage """
-        prediction = self.Prediction(contextual_feature.contiguous())
+        if self.stages['Pred'] == 'CTC':
+            prediction = self.Prediction(contextual_feature.contiguous())
+        # else:
+        #     prediction = self.Prediction(contextual_feature.contiguous(), text, is_train, batch_max_length=self.opt.batch_max_length)
 
         return prediction
